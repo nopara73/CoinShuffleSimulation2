80b7cf3c7610f41dceeb5ae96e78245c95d7259b
nopara73 <adam.ficsor73@gmail.com>
2020-01-16 15:18:20 -0400

    Add project files.

---
+++ CoinShuffleSimulation2/Peer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoinShuffleSimulation2
+{
+    internal class Peer
+    {
+        public Peer(string name)
+        {
+            Name = name;
+        }
+
+        public event EventHandler<string> NewMessage;
+
+        public string Name { get; }
+
+        private List<Peer> Connections { get; } = new List<Peer>();
+        private List<string> ProcessedMessages { get; } = new List<string>();
+
+        internal void Connect(Peer peer)
+        {
+            if (Connections.Contains(peer)) return;
+            Connections.Add(peer);
+            Console.WriteLine($"{Name} connected to {peer.Name}.");
+
+            peer.NewMessage += Peer_NewMessage;
+            peer.Connect(this);
+        }
+
+        internal void Broadcast(string message)
+        {
+            NewMessage?.Invoke(this, message);
+        }
+
+        private void Peer_NewMessage(object sender, string message)
+        {
+            if (ProcessedMessages.Contains(message)) return;
+            ProcessedMessages.Add(message);
+            Console.Write($"{Name} received message: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{message}");
+            Console.ResetColor();
+
+            NewMessage?.Invoke(this, message);
+        }
+    }
+}
---
+++ CoinShuffleSimulation2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CoinShuffleSimulation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alice = new Peer("Alice");
+            var bob = new Peer("Bob");
+            alice.Connect(bob);
+            var satoshi = new Peer("Satoshi");
+            satoshi.Connect(bob);
+
+            alice.Broadcast("Hello P2P World!");
+
+            Console.WriteLine("Press a key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
